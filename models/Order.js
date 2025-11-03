86f410e9ed71a2a44ee2161c9198ad2ff99b02cb
Gabriel <saralimaabm@gmail.com>
2025-11-03 16:30:54 -0300

    Atualiza Order.js para salvar created_at no horário de Brasília

--- models/Order.js
+++ models/Order.js
@@ -7,7 +7,15 @@ const OrderSchema = new mongoose.Schema({
   quantity: { type: Number, required: true },
   remains: { type: Number, required: true },
   status: { type: String, default: "pending" }, // pending, completed, cancelled
-  created_at: { type: Date, default: Date.now },
+  //created_at: { type: Date, default: Date.now },
+  created_at: {
+    type: String,
+    default: () =>
+      new Date().toLocaleString("pt-BR", {
+        timeZone: "America/Sao_Paulo",
+        hour12: false
+      })
+  }
 });
 
 export default mongoose.model("Order", OrderSchema);
